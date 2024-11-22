70f220bd2e3ba8a49b9530ba019d182d51500601
王思閎 <wangszuhung1@gmail.com>
2024-11-22 09:41:36 +0800

    ADD install-service 背景處理，保護斷線機制

--- frpClient/install-service.sh
+++ frpClient/install-service.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-systemctl stop frp-client.service
-cp frp-client.service /etc/systemd/system/frp-client.service
-systemctl daemon-reload
-systemctl start frp-client.service
-systemctl enable frp-client.service
+# install-service.sh
+nohup bash -c "
+  systemctl stop frp-client.service
+  cp frp-client.service /etc/systemd/system/frp-client.service
+  systemctl daemon-reload
+  systemctl start frp-client.service
+  systemctl enable frp-client.service
+" > /tmp/install-service.log 2>&1 &
